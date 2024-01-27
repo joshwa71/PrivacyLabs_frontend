93d7bc797a7ac20836296e1442d13ead80c36c8e
joshwa71 <joshuaohara71@gmail.com>
2024-01-27 11:29:17 +0000

    new routes and stuff

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function sendBuildL2Request() {
     const company = document.getElementById('l2-company').value;
-    const industry = document.getElementById('l2-industry').value;
 
-    fetch('https://privacy-labs-graphs-6bz7xclj5a-wl.a.run.app/build_l2', {
+    fetch('https://privacy-labs-demo-6bz7xclj5a-wl.a.run.app/build_l2', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ company, industry }),
+        body: JSON.stringify({ company }),
     })
     .then(response => response.json())
     .then(data => {
@@ -20,15 +19,14 @@ function sendBuildL2Request() {
 
 function sendBuildL3Request() {
     const company = document.getElementById('l3-company').value;
-    const industry = document.getElementById('l3-industry').value;
     const urls = document.getElementById('l3-urls').value.split(',');
 
-    fetch('https://privacy-labs-graphs-6bz7xclj5a-wl.a.run.app/build_l3', {
+    fetch('https://privacy-labs-demo-6bz7xclj5a-wl.a.run.app/build_l3', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ company, industry, urls }),
+        body: JSON.stringify({ company, urls }),
     })
     .then(response => response.json())
     .then(data => {
@@ -41,17 +39,15 @@ function sendBuildL3Request() {
 
 function sendCompareRequest() {
     const company1 = document.getElementById('company1').value;
-    const industry1 = document.getElementById('industry1').value;
     const company2 = document.getElementById('company2').value;
-    const industry2 = document.getElementById('industry2').value;
     const term = document.getElementById('term').value;
 
-    fetch('https://privacy-labs-graphs-6bz7xclj5a-wl.a.run.app/compare_terms', {
+    fetch('https://privacy-labs-demo-6bz7xclj5a-wl.a.run.app/compare_terms', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ company1, industry1, company2, industry2, term }),
+        body: JSON.stringify({ company1, company2, term }),
     })
     .then(response => response.json())
     .then(data => {
@@ -64,21 +60,59 @@ function sendCompareRequest() {
 
 function sendQueryRequest() {
     const company = document.getElementById('query-company').value;
-    const industry = document.getElementById('query-industry').value;
     const layer = document.getElementById('query-layer').value;
     const query = document.getElementById('query-query').value;
     const mode = document.getElementById('query-mode').value;
 
-    fetch('https://privacy-labs-graphs-6bz7xclj5a-wl.a.run.app/query', {
+    fetch('https://privacy-labs-demo-6bz7xclj5a-wl.a.run.app/query', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ company, industry, layer, query, mode }),
+        body: JSON.stringify({ company, layer, query, mode }),
     })
     .then(response => response.json())
     .then(data => {
         document.getElementById('query-response').innerText = JSON.stringify(data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function sendGetNodesRequest() {
+    const company = document.getElementById('get-nodes-company').value;
+    const layer = document.getElementById('get-nodes-layer').value;
+
+    fetch('https://privacy-labs-demo-6bz7xclj5a-wl.a.run.app/get_nodes', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ company, layer }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('get-nodes-response').innerText = JSON.stringify(data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function sendGetNodeCountRequest() {
+    const layer = document.getElementById('get-node-count-layer').value;
+
+    fetch('https://privacy-labs-demo-6bz7xclj5a-wl.a.run.app/get_node_count', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ layer }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('get-node-count-response').innerText = JSON.stringify(data);
     })
     .catch((error) => {
         console.error('Error:', error);
